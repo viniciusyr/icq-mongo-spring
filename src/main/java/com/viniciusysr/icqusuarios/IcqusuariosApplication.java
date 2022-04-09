89d2cdb86fc50c38b502b9a619c3e84c7603a2c8
Vinicius Rodrigues <vinicius.worms@hotmail.com>
2022-04-09 15:30:10 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/viniciusysr/icqusuarios/IcqusuariosApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusysr.icqusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcqusuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcqusuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciusysr/icqusuarios/IcqusuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusysr.icqusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcqusuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
